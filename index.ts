c9f70ba19117f9132ada22d4dd6becfad1e5843d
Yurii Soroka <yurisoroka165@gmail.com>
2023-12-31 21:34:21 +0200

    try to login

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log(1);
+import axios, { AxiosResponse } from "axios";
+import * as https from "https";
+
+const agent = new https.Agent({ rejectUnauthorized: false });
+
+axios
+    .post(
+        "https://192.168.30.174:8443/api/login",
+        {
+            username: "admin",
+            password: "admin330691",
+        },
+        { httpsAgent: agent }
+    )
+    .then(response => console.log(response));
